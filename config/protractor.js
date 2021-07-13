21a83ce64da8d45ec8e81b9707093b551ceb1905
danilo.romano <danilo.romano@azi.com.br>
2021-07-13 15:43:25 -0400

    Integração de teste com Sauce Labs

--- config/protractor.js
+++ config/protractor.js
@@ -1,10 +1,18 @@
-var config=require('./config.js')();
+var config = require('./config.js')();
 
 exports.config = {
+    sauceUser: config.sauceUser,
+    saudeKey: config.saudeKey,
+    capabilities: {
+        'name': config.sauceTestName,
+        'browserName': 'chrome',
+        'tunnel-identifier': config.travisJobNumber,
+        'build': config.travisBuild
+    },
     specs: ['../test/e2e/**/*.js'],
     onPrepare: function () {
         browser.driver.get('http://localhost:3000')
-            .then(function(){
+            .then(function () {
                 browser.driver.findElement(by.id('entrar')).click();
                 browser.driver.findElement(by.id('login_field'))
                     .sendKeys(config.seleniumUser);
