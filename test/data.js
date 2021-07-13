34de915fec4b01ac9db9d4b588c4cc9e614efc8a
danilo.romano <danilo.romano@azi.com.br>
2021-07-13 16:46:53 -0400

    ajutando a carga de dados, estava com erro.

--- test/data.js
+++ test/data.js
@@ -1,24 +1,49 @@
-var MongoClient = require('mongodb').MongoClient;
+var mongoose = require("mongoose");
 
-var contatos= [
-    {nome: 'xyz1',email:'xyz1@email.com'},
-    {nome: 'xyz2',email:'xyz2@email.com'},
-    {nome: 'xyz3',email:'xyz3@email.com'},
-    {nome: 'xyz4',email:'xyz4@email.com'}
+var schema = mongoose.Schema({
+    nome: {
+        type: String,
+        required: true
+    },
+    email: {
+        type: String,
+        required: true,
+        index: {
+            unique: true
+        }
+    },
+    emergencia: {
+        type: mongoose.Schema.ObjectId,
+        ref: 'Contato'
+    }
+});
+
+var ContatoModel = mongoose.model('Contato', schema);
+
+
+var contatos = [
+    {nome: 'xyz1', email: 'xyz1@email.com'},
+    {nome: 'xyz2', email: 'xyz2@email.com'},
+    {nome: 'xyz3', email: 'xyz3@email.com'},
+    {nome: 'xyz4', email: 'xyz4@email.com'}
 ];
+const conn = mongoose.createConnection('mongodb://localhost:27017/contatooh_test');
+// Deletes the entire 'mydb' database
+conn.dropDatabase();
 
-MongoClient.connect('mongodb://127.0.0.1:27017/contatooh_test',
-    function(erro,db){
-        if (erro) throw err;
+contatos.forEach((c) => {
+    ContatoModel.create(c)
+        .then(function () {
+            console.log("Usuario inserido");
+        }, function (err) {
+            console.log(err);
+        });
+});
 
-        db.dropDatabase(function(err){
-            if (err) return console.log(err);
-            console.log('Banco apagado com sucesso');
-            db.collection('contatos').insert(contatos,
-                function(err,inserted){
-                    if (err) return console.log(err);
-                    console.log('Banco populado com sucesso');
-                    process.exit(0);
-                });
-        })
-    });
+conn.close(true).then(
+    function(){
+        console.log('banco fechado');
+    }
+);
+
+process.exit(0);
