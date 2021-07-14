989a26db15d8f1f17e0eda4536fb309956a2aa85
danilo.romano <danilo.romano@azi.com.br>
2021-07-14 17:15:21 -0400

    Ajustando karma.config.js para rodar com o phantom

--- test/spec/ContatoControllerSpec.js
+++ test/spec/ContatoControllerSpec.js
@@ -11,22 +11,22 @@ describe('ContatoController', function () {
         });
     });
 
-    // it(
-    //     "Deve criar um Contato vazio quando nenhum parâmetro de rota for passado",
-    //     inject(function ($controller) {
-    //         $controller('ContatoController', {"$scope": $scope});
-    //         expect($scope.contato._id).toBeUndefined();
-    //     }));
-    //
-    // it("Deve preencher o Contato quando parâmetro  de rota for passado",
-    //     inject(function ($controller) {
-    //         $controller('ContatoController', {
-    //             $routeParams: {contatoId: 1},
-    //             '$scope': $scope
-    //         });
-    //         $httpBackend.flush();
-    //         expect($scope.contato._id).toBeDefined();
-    //     })
-    // )
-    // ;
+    it(
+        "Deve criar um Contato vazio quando nenhum parâmetro de rota for passado",
+        inject(function ($controller) {
+            $controller('ContatoController', {"$scope": $scope});
+            expect($scope.contato._id).toBeUndefined();
+        }));
+
+    it("Deve preencher o Contato quando parâmetro  de rota for passado",
+        inject(function ($controller) {
+            $controller('ContatoController', {
+                $routeParams: {contatoId: 1},
+                '$scope': $scope
+            });
+            $httpBackend.flush();
+            expect($scope.contato._id).toBeDefined();
+        })
+    )
+    ;
 });
